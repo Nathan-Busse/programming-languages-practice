d5a94b833a4b74dbe10eea705c9934af21903f04
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2024-10-24 07:54:30 +0200

    Hello, World!

---
+++ Codeing practise/c/hello-world.c
@@ -0,0 +1,37 @@
+/*
+My very first program:
+Date created: 22 October 2024
+Author: Nathan-Busse
+program info:
+
+    Print Hello, World! as an output onto the console.
+
+*/
+
+#include<stdio.h>
+
+int main(void){
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/*
+Code syntax explaination:
+-------------------------
+
+#include is where we call a <preprogram> into our source script.
+
+int or intager is a data type attribute that we use to assign a function.
+
+main is our name type attribute that acts as our intagers identifer.
+
+void in this case is our value for our variable.
+
+printf is a function that allows us to print text onto the console during runtime.
+
+return is use to crearte whats calledd a an exception funvtion/
+
+An exception function is used to genereate messahes messages during runtime if the program excecuted with or without any problems.
+
+*/
+
